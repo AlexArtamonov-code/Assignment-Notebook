3f43e095d7828e00b0a47832af624299490fa55b
Alex Artamonov <aartamonov@bsd220.org>
2026-02-25 10:38:11 -0600

    Initial Commit

---
+++ Assignment Notebook/Assignment_NotebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_NotebookApp.swift
+//  Assignment Notebook
+//
+//  Created by Alex Artamonov on 2/25/26.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_NotebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
