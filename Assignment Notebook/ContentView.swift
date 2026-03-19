0efd65a2b54c0a508db08ce058cea618e8582faf
Alex Artamonov <aartamonov@bsd220.org>
2026-03-18 21:30:17 -0500

    Added custom struct and array

--- Assignment Notebook/ContentView.swift
+++ Assignment Notebook/ContentView.swift
@@ -8,6 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var assignmentItems = [
+        AssignmentItem(course: "Algebra", description: "Linear Equation", dueDate: Date()),
+        AssignmentItem(course: "History", description: "Civil War Paper", dueDate: Date()),
+        AssignmentItem(course: "Science", description: "Atomic Bomb Lab", dueDate: Date())
+    ]
+    struct AssignmentItem: Identifiable {
+        var id = UUID()
+        var course: String
+        var description: String
+        var dueDate: Date
+    }
     var body: some View {
         VStack {
             Image(systemName: "globe")
