784a7638e439b004c3fa43a820e6d63639d261bd
Alex Artamonov <aartamonov@bsd220.org>
2026-03-19 10:04:09 -0500

    Made finishing touches

--- Assignment Notebook/AssignmentList.swift
+++ Assignment Notebook/AssignmentList.swift
@@ -9,9 +9,30 @@ import Foundation
 import Combine
 
 class AssignmentList: ObservableObject {
-    @Published var items: [AssignmentItem] = [
-        AssignmentItem(course: "Algebra", description: "Linear Equation", dueDate: Date()),
-        AssignmentItem(course: "History", description: "Civil War Paper", dueDate: Date()),
-        AssignmentItem(course: "Science", description: "Atomic Bomb Lab", dueDate: Date())
-    ]
+    
+    @Published var items: [AssignmentItem] {
+        didSet {
+            save()
+        }
+    }
+    let saveKey = "AssignmentItems"
+    
+    init() {
+        if let data = UserDefaults.standard.data(forKey: saveKey),
+           let decoded = try? JSONDecoder().decode([AssignmentItem].self, from: data) {
+            items = decoded
+        } else {
+            items = [
+                AssignmentItem(course: "Algebra", description: "Linear Equation", dueDate: Date()),
+                AssignmentItem(course: "History", description: "Civil War Paper", dueDate: Date()),
+                AssignmentItem(course: "Science", description: "Atomic Bomb Lab", dueDate: Date())
+            ]
+        }
+    }
+    
+    func save() {
+        if let encoded = try? JSONEncoder().encode(items) {
+            UserDefaults.standard.set(encoded, forKey: saveKey)
+        }
+    }
 }
